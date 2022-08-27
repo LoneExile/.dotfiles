0b1d4e6142831a4916711ebf5f3df6613a63b010
Wolowit <apinant-moo@tutanota.com>
2022-08-27 21:37:43 +0700

    lvim update lspsaga config

--- dot_config/lvim/lua/user/lsp/config.lua
+++ dot_config/lvim/lua/user/lsp/config.lua
@@ -7,7 +7,7 @@
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
 -- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
+-- require("lvim.lsp.manager").setup("pylsp", opts)
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
 -- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
