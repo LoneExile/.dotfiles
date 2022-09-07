4c26e950d5dc7b944e3cc8ece970683498523cc2
Wolowit <apinant-moo@tutanota.com>
2022-09-07 21:17:21 +0700

    minor update

--- dot_config/nvim/lua/user/lsp/null-ls.lua
+++ dot_config/nvim/lua/user/lsp/null-ls.lua
@@ -1,23 +1,23 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
 local config = {
-  debug = false,
-  sources = {
-    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-      filetypes = { "typescript", "typescriptreact" } }),
-    formatting.black.with({ extra_args = { "--fast" }, filetypes = { "python" } }),
-    formatting.stylua,
-    diagnostics.flake8,
-    formatting.isort.with({ filetypes = { "python" } })
-  },
+	debug = false,
+	sources = {
+		formatting.prettier.with({
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+			filetypes = { "typescript", "typescriptreact" },
+		}),
+		formatting.black.with({ extra_args = { "--fast" }, filetypes = { "python" } }),
+		formatting.stylua,
+		diagnostics.flake8,
+		formatting.isort.with({ filetypes = { "python" } }),
+	},
 }
 
 null_ls.setup(config)
